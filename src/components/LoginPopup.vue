8e9a831dd4f844236c661b26d5ce6de1a2dbd5a4
KaiChen <kai.chen@opennet.tw>
2024-08-29 00:11:52 +0800

    complete register and login

--- src/components/LoginPopup.vue
+++ src/components/LoginPopup.vue
@@ -6,8 +6,12 @@
             </template>
             <template v-slot:subtitle>
                 <span>Welcome!</span>
+                <v-switch v-model="data.isRegister" color="green" label="Register" hide-details
+                    class="ma-5 position-absolute top-0 right-0"></v-switch>
             </template>
-
+            <v-card-text v-if="data.greetings" class="d-flex justify-center align-center">
+                {{ data.greetings }}
+            </v-card-text>
             <v-card-text>
                 <v-sheet class="mx-auto" width="300">
                     <v-form @submit.prevent>
@@ -17,8 +21,8 @@
                             @click:append-inner="data.showPassword = !data.showPassword"
                             :append-inner-icon="data.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                             :type="data.showPassword ? 'text' : 'password'" label="password"></v-text-field>
-                        <v-btn color="primary" type="submit" @click="login" block class="my-3" height="80"
-                            :disabled="!isLegalLoginForm()">Login</v-btn>
+                        <v-btn color="primary" type="submit" @click="onMainBtnClick" block class="my-3" height="80"
+                            :disabled="!isLegalLoginForm() || data.autoLoginProcess">{{ getMainBtnLabel() }}</v-btn>
                         <v-btn color="grey" @click="props.onCancelClick" block class="my-3">Cancel</v-btn>
                     </v-form>
                 </v-sheet>
@@ -46,12 +50,18 @@ const props = defineProps({
 
 const data = ref({
     greetings: null,
+    isRegister: false,
+    autoLoginProcess: false,
     inputAccount: '',
     inputPassword: '',
     showPassword: false,
     accountRules: inputTextRules.accountRules,
     passwordRules: inputTextRules.passwordRules,
 });
+
+function getMainBtnLabel() {
+    return data.value.isRegister ? "Register" : "Login";
+}
 
 function isLegalLoginForm() {
     const { inputAccount, inputPassword, accountRules, passwordRules } = data.value;
@@ -60,17 +70,35 @@ function isLegalLoginForm() {
     return isAccountValid && isPasswordValid;
 }
 
-async function login() {
+function getMainBtnUrl() {
+    return data.value.isRegister ? "/user/create" : "/user/createToken";
+}
+
+async function onMainBtnClick() {
     try {
         const inputAccount = data.value.inputAccount;
         const inputPassword = data.value.inputPassword;
-        const tokenInfo = await api.post('/user/createToken', { account: inputAccount, password: inputPassword });
-        const token = tokenInfo ? tokenInfo.token : null;
-        const tokenExpiredTime = tokenInfo ? tokenInfo.expiredTime : null;
-        if (token) {
-            localStorage.setItem('localStorageUser', JSON.stringify({ account: inputAccount, loginToken: token, loginTokenExpiredTime: tokenExpiredTime }));
+        const result = await api.post(getMainBtnUrl(), { account: inputAccount, password: inputPassword });
+
+        if (result) {
+            // register success, auto call login
+            if (data.value.isRegister) {
+                data.value.isRegister = false;
+                data.value.autoLoginProcess = true;
+                onMainBtnClick();
+            } else {
+                const token = result ? result.token : null;
+                const tokenExpiredTime = result ? result.expiredTime : null;
+                if (token) {
+                    localStorage.setItem('localStorageUser', JSON.stringify({ account: inputAccount, loginToken: token, loginTokenExpiredTime: tokenExpiredTime }));
+                }
+            }
+
             props.onLoginSuccess();
+        } else {
+            data.value.autoLoginProcess = false;
         }
+
     } catch (err) {
         console.error(err);
 
