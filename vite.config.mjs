d5bfb4afc8c8b7c982983f72a9b17b9bee145678
Kai <akai1024@hotmail.com>
2024-09-05 00:10:33 +0800

    resume setting for github pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/kai-game-front/',
+  base:'/kai-game-front/',
   plugins: [
     VueRouter(),
     Vue({
