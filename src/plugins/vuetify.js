126438804f48bd587c8dc25aad1895202d4255fb
Kai <akai1024@hotmail.com>
2025-10-26 11:27:08 +0800

    test commit

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,7 +14,7 @@ import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  // blueprint: md1,
+  // blueprint: md1, test commit
   theme: {
     defaultTheme: 'dark',
   },
