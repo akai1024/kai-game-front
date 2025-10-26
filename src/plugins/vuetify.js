5aeb8c80812d81003a5e9f8c5454505f4581bf24
Kai <akai1024@hotmail.com>
2025-10-26 11:29:06 +0800

    revert

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,7 +14,7 @@ import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  // blueprint: md1, test commit
+  // blueprint: md1
   theme: {
     defaultTheme: 'dark',
   },
